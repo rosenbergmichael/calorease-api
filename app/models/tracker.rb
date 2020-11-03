30360a47daae59b9873b899496e30a6ffbcba5a3
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-11-03 16:16:30 -0500

    set up update_cal method in tracker model which will be used for counting calories as foods are added

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,6 +1,25 @@
 class Tracker < ApplicationRecord
   has_many :foods
-
   validates :day, :date, presence: true
 
+  def update_cal(food)
+    self.totalcal = self.totalcal + food.calories
+    self.save
+  end
+
 end
+
+
+
+create_table "trackers", force: :cascade do |t|
+  t.string "day"
+  t.datetime "date"
+  t.float "totalcal"
+
+
+create_table "foods", force: :cascade do |t|
+  t.integer "tracker_id"
+  t.string "name"
+  t.float "calories"
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tracker = Tracker.create(day: 'Tuesday', date: Date.today, totalcal: 0)
+
+food = Food.create(tracker_id: 1, name: 'apple', calories: 50)
+
+food_two = Food.create(tracker_id: 1, name: 'soup', calories: 250)
+
+food_three = Food.create(tracker_id: 1, name: 'ice cream')
