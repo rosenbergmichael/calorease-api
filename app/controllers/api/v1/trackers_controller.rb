6d1f92de85b8b3cd4dfe91cd49224704c1b282b1
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-11-03 15:12:58 -0500

    added api v1 folder for controllers and namespaced routes and controllers accordingly

---
+++ app/controllers/api/v1/trackers_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::TrackersController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :foods
-  resources :trackers
+  namespace :api do
+    namespace :v1 do 
+      resources :foods
+      resources :trackers
+    end
+  end 
+
 end
+ 
